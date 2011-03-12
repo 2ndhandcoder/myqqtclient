d4a7b60275a7b180762c9878192b832e82204742
二手攻城师 <2ndhandcoder@gmail.com>
2011-03-12 17:31:49 +0800

    工厂类

--- QQTClient/QQTClient/CFactory.cs
+++ QQTClient/QQTClient/CFactory.cs
@@ -1,11 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using OpenTSDK.Tencent;
 
 namespace QQTClient.Logic
 {
     public class CFactory
     {
-        private static 
+        private static string _appKey = "474173220d3443748d4caf9a84d9c5b5";
+        private static string _appSecret = "c18c32903485c9a54b9f76e997fcac0f";
+        private static OAuth _Auth;
+
+        /// <summary>
+        /// 应用key
+        /// </summary>
+        public static string appKey
+        {
+            get
+            {
+                return _appKey;
+            }
+        }
+
+        /// <summary>
+        /// 应用secret
+        /// </summary>
+        public static string appSecret
+        {
+            get
+            {
+                return _appSecret;
+            }
+        }
+
+        /// <summary>
+        /// 验证对象
+        /// </summary>
+        public static OAuth Auth
+        {
+            get
+            {
+                if (null == _Auth)
+                    _Auth =  new OAuth(_appKey, _appSecret);
+                return _Auth;
+            }
+        }
     }
 }
